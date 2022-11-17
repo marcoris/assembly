e4ce6c94cf48dd2f2f653e1a4cf461e749f2df36
Marco Ris <marco.ris@risdesign.ch>
2022-11-17 21:54:44 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-rm src/*.{o, nes} && \
+rm src/*.o helloworld.nes && \
 ca65 src/helloworld.asm && \
 ca65 src/reset.asm && \
 ld65 src/reset.o src/helloworld.o -C nes.cfg -o helloworld.nes
