876ddca6121168f7022e4d9215e003e2ad9a4368
Marco Ris <ris@contria.com>
2022-11-18 22:31:11 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-rm src/*.o spritegraphics.nes && \
+rm -rf src/*.o *.nes && \
 ca65 src/sprites.asm && \
 ca65 src/reset.asm && \
 ld65 src/reset.o src/sprites.o -C nes.cfg -o spritegraphics.nes
