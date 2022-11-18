74c63cab76fdd5e36ac2669f53a0885a83416032
Marco Ris <marco.ris@risdesign.ch>
2022-11-18 22:32:49 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 rm -rf src/*.o *.nes && \
-ca65 src/sprites.asm && \
+ca65 src/main.asm && \
 ca65 src/reset.asm && \
-ld65 src/reset.o src/sprites.o -C nes.cfg -o spritegraphics.nes
+ld65 src/reset.o src/main.o -C nes.cfg -o main.nes
