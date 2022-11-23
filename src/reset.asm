dfd92179a6dd2a669c7207910a6550e2d3c53dbf
Marco Ris <marco.ris@risdesign.ch>
2022-11-23 21:20:17 +0100

    Improve code

--- src/reset.asm
+++ src/reset.asm
@@ -9,9 +9,24 @@
     LDX #$00
     STX PPUCTRL
     STX PPUMASK
+
 vblankwait:
     BIT PPUSTATUS
     BPL vblankwait
+
+    LDX #$00
+	LDA #$ff
+clear_oam:
+	STA $0200,X ; set sprite y-positions off the screen
+	INX
+	INX
+	INX
+	INX
+	BNE clear_oam
+
+vblankwait2:
+	BIT PPUSTATUS
+	BPL vblankwait2
 
     JMP main
 .endproc
