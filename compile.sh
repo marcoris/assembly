e5760093378b62478fe29cc9dfab6714cc633467
Marco Ris <marco.ris@risdesign.ch>
2023-02-24 22:22:25 +0100

    Fix compiler

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 rm -rf src/*.o *.nes && \
+ca65 src/backgrounds.asm && \
 ca65 src/main.asm && \
 ca65 src/reset.asm && \
-ld65 src/reset.o src/main.o -C nes.cfg -o main.nes
+ld65 src/backgrounds.o src/reset.o src/main.o -C nes.cfg -o main.nes
