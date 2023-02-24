2bcd3b26246c22eddf9adacc6cf72c6f057326e3
Marco Ris <marco.ris@risdesign.ch>
2023-02-24 22:25:50 +0100

    Fix indentation

--- src/main.asm
+++ src/main.asm
@@ -11,31 +11,31 @@ ppuctrl_settings: .res 1
 
 .segment "CODE"
 .proc irq_handler
-  RTI
+    RTI
 .endproc
 
 .proc nmi_handler
-  LDA #$00
-  STA OAMADDR
-  LDA #$02
-  STA OAMDMA
-	LDA #$00
+    LDA #$00
+    STA OAMADDR
+    LDA #$02
+    STA OAMDMA
+    LDA #$00
 
-  ; update tiles *after* DMA transfer
-	JSR update_player
-  JSR draw_player
+    ; update tiles *after* DMA transfer
+    JSR update_player
+    JSR draw_player
 
-	LDA scroll
-	CMP #$00 ; did we scroll to the end of a nametable?
-	BNE set_scroll_positions
-	; if yes,
-	; Update base nametable
-	LDA ppuctrl_settings
-	EOR #%00000010 ; flip bit 1 to its opposite
-	STA ppuctrl_settings
-	STA PPUCTRL
-	LDA #240
-	STA scroll
+    LDA scroll
+    CMP #$00 ; did we scroll to the end of a nametable?
+    BNE set_scroll_positions
+    ; if yes,
+    ; Update base nametable
+    LDA ppuctrl_settings
+    EOR #%00000010 ; flip bit 1 to its opposite
+    STA ppuctrl_settings
+    STA PPUCTRL
+    LDA #240
+    STA scroll
 
 set_scroll_positions:
 	LDA #$00 ; X scroll first
@@ -44,7 +44,7 @@ set_scroll_positions:
 	LDA scroll ; then Y scroll
 	STA PPUSCROLL
 
-  RTI
+    RTI
 .endproc
 
 .import reset_handler
@@ -56,18 +56,18 @@ set_scroll_positions:
 	LDA #239	 ; Y is only 240 lines tall!
 	STA scroll
 
-  ; write a palette
-  LDX PPUSTATUS
-  LDX #$3f
-  STX PPUADDR
-  LDX #$00
-  STX PPUADDR
+    ; write a palette
+    LDX PPUSTATUS
+    LDX #$3f
+    STX PPUADDR
+    LDX #$00
+    STX PPUADDR
 load_palettes:
-  LDA palettes,X
-  STA PPUDATA
-  INX
-  CPX #$20
-  BNE load_palettes
+    LDA palettes,X
+    STA PPUDATA
+    INX
+    CPX #$20
+    BNE load_palettes
 
 	; write nametables
 	LDX #$20
@@ -79,133 +79,133 @@ load_palettes:
 	JSR draw_objects
 
 vblankwait:       ; wait for another vblank before continuing
-  BIT PPUSTATUS
-  BPL vblankwait
+    BIT PPUSTATUS
+    BPL vblankwait
 
-  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
-	STA ppuctrl_settings
-  STA PPUCTRL
-  LDA #%00011110  ; turn on screen
-  STA PPUMASK
+    LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+    STA ppuctrl_settings
+    STA PPUCTRL
+    LDA #%00011110  ; turn on screen
+    STA PPUMASK
 
 forever:
-  JMP forever
+    JMP forever
 .endproc
 
 .proc update_player
-  PHP
-  PHA
-  TXA
-  PHA
-  TYA
-  PHA
+    PHP
+    PHA
+    TXA
+    PHA
+    TYA
+    PHA
 
-  LDA player_x
-  CMP #$e0
-  BCC not_at_right_edge
-  ; if BCC is not taken, we are greater than $e0
-  LDA #$00
-  STA player_dir    ; start moving left
-  JMP direction_set ; we already chose a direction,
+    LDA player_x
+    CMP #$e0
+    BCC not_at_right_edge
+    ; if BCC is not taken, we are greater than $e0
+    LDA #$00
+    STA player_dir    ; start moving left
+    JMP direction_set ; we already chose a direction,
                     ; so we can skip the left side check
 not_at_right_edge:
-  LDA player_x
-  CMP #$10
-  BCS direction_set
-  ; if BCS not taken, we are less than $10
-  LDA #$01
-  STA player_dir   ; start moving right
+    LDA player_x
+    CMP #$10
+    BCS direction_set
+    ; if BCS not taken, we are less than $10
+    LDA #$01
+    STA player_dir   ; start moving right
 direction_set:
-  ; now, actually update player_x
-  LDA player_dir
-  CMP #$01
-  BEQ move_right
-  ; if player_dir minus $01 is not zero,
-  ; that means player_dir was $00 and
-  ; we need to move left
-  DEC player_x
-  JMP exit_subroutine
+    ; now, actually update player_x
+    LDA player_dir
+    CMP #$01
+    BEQ move_right
+    ; if player_dir minus $01 is not zero,
+    ; that means player_dir was $00 and
+    ; we need to move left
+    DEC player_x
+    JMP exit_subroutine
 move_right:
-  INC player_x
+    INC player_x
 exit_subroutine:
-  ; all done, clean up and return
-  PLA
-  TAY
-  PLA
-  TAX
-  PLA
-  PLP
-  RTS
+    ; all done, clean up and return
+    PLA
+    TAY
+    PLA
+    TAX
+    PLA
+    PLP
+    RTS
 .endproc
 
 .proc draw_player
-  ; save registers
-  PHP
-  PHA
-  TXA
-  PHA
-  TYA
-  PHA
+    ; save registers
+    PHP
+    PHA
+    TXA
+    PHA
+    TYA
+    PHA
 
-  ; write player ship tile numbers
-  LDA #$05
-  STA $0201
-  LDA #$06
-  STA $0205
-  LDA #$07
-  STA $0209
-  LDA #$08
-  STA $020d
+    ; write player ship tile numbers
+    LDA #$05
+    STA $0201
+    LDA #$06
+    STA $0205
+    LDA #$07
+    STA $0209
+    LDA #$08
+    STA $020d
 
-  ; write player ship tile attributes
-  ; use palette 0
-  LDA #$00
-  STA $0202
-  STA $0206
-  STA $020a
-  STA $020e
+    ; write player ship tile attributes
+    ; use palette 0
+    LDA #$00
+    STA $0202
+    STA $0206
+    STA $020a
+    STA $020e
 
-  ; store tile locations
-  ; top left tile:
-  LDA player_y
-  STA $0200
-  LDA player_x
-  STA $0203
+    ; store tile locations
+    ; top left tile:
+    LDA player_y
+    STA $0200
+    LDA player_x
+    STA $0203
 
-  ; top right tile (x + 8):
-  LDA player_y
-  STA $0204
-  LDA player_x
-  CLC
-  ADC #$08
-  STA $0207
+    ; top right tile (x + 8):
+    LDA player_y
+    STA $0204
+    LDA player_x
+    CLC
+    ADC #$08
+    STA $0207
 
-  ; bottom left tile (y + 8):
-  LDA player_y
-  CLC
-  ADC #$08
-  STA $0208
-  LDA player_x
-  STA $020b
+    ; bottom left tile (y + 8):
+    LDA player_y
+    CLC
+    ADC #$08
+    STA $0208
+    LDA player_x
+    STA $020b
 
-  ; bottom right tile (x + 8, y + 8)
-  LDA player_y
-  CLC
-  ADC #$08
-  STA $020c
-  LDA player_x
-  CLC
-  ADC #$08
-  STA $020f
+    ; bottom right tile (x + 8, y + 8)
+    LDA player_y
+    CLC
+    ADC #$08
+    STA $020c
+    LDA player_x
+    CLC
+    ADC #$08
+    STA $020f
 
-  ; restore registers and return
-  PLA
-  TAY
-  PLA
-  TAX
-  PLA
-  PLP
-  RTS
+    ; restore registers and return
+    PLA
+    TAY
+    PLA
+    TAX
+    PLA
+    PLP
+    RTS
 .endproc
 
 .segment "VECTORS"
